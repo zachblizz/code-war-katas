64687aee7e99fc53ffbff5ef6d04afc32ec3b977
Zach.Blizzard <zach.blizzard@solera.com>
2020-01-07 14:23:32 -0600

    optimize slice sort comparator

--- merge-intervals/main.go
+++ merge-intervals/main.go
@@ -11,13 +11,7 @@ func merge(intervals [][]int) [][]int {
 	}
 
 	sort.Slice(intervals[:], func(i, j int) bool {
-		for x := range intervals[i] {
-			if intervals[i][x] == intervals[j][x] {
-				continue
-			}
-			return intervals[i][x] < intervals[j][x]
-		}
-		return false
+		return intervals[i][0] < intervals[j][0]
 	})
 
 	return helper(intervals, [][]int{}, intervals[0], intervals[0], 0)
@@ -52,26 +46,6 @@ func main() {
 	intervals := [][]int{
 		[]int{1,4},
 		[]int{0,1},
-	}
-
-	// intervals = [][]int{
-	// 	[]int{1,3},
-	// 	[]int{2,8},
-	// 	[]int{8,10},
-	// 	[]int{10,18},
-	// }
-
-	// intervals = [][]int{
-	// 	[]int{1,4},
-	// 	[]int{0,0},
-	// }
-	
-	intervals = [][]int{
-		[]int{2,3},
-		[]int{4,5},
-		[]int{6,7},
-		[]int{8,9},
-		[]int{1,10},
 	}
 
 	fmt.Println(merge(intervals))
