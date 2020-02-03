81675f8c25e97f4959daee9094ed9fbe50c304d4
Zach.Blizzard <zach.blizzard@solera.com>
2020-02-03 16:00:05 -0600

    so we can do things more like leet code... 👍👍✌😢 still not fixed...

--- lru-cache/main.go
+++ lru-cache/main.go
@@ -22,13 +22,11 @@ func Constructor(capacity int) LRUCache {
 // also should update everyone's lruTime
 func (l *LRUCache) Get(key int) int {
 	if _, ok := l.lruCache[key]; ok {
-		newTimes := 1
 		l.lruCache[key] = 0
 
 		for k := range l.lruCache {
 			if k != key {
-				l.lruCache[k] = newTimes
-				newTimes++
+				l.lruCache[k]++
 			}
 		}
 	}
@@ -50,7 +48,7 @@ func (l *LRUCache) Put(key int, value int) {
 
 	if l.lruLen < l.cap {
 		l.lruLen++
-	} else {
+	} else if _, ok := l.lruCache[key]; !ok {
 		var remK int
 		maxTime := -1
 
@@ -60,7 +58,7 @@ func (l *LRUCache) Put(key int, value int) {
 				remK = k
 			}
 		}
-		fmt.Printf("going to remove %v\n", remK)
+		// fmt.Printf("going to remove %v\n", remK)
 
 		// remove the key from the cache
 		delete(l.cache, remK)
@@ -71,55 +69,44 @@ func (l *LRUCache) Put(key int, value int) {
 	l.cache[key] = value
 }
 
-func tryTwo() {
-	cache := Constructor(2)
+func setup(ops []string, vals [][]int) {
+	var cache LRUCache
+	out := []int{}
 
-	cache.Put(1, 1)
-	cache.Put(2, 2)
-	fmt.Println(cache.Get(1))
-	cache.Put(3, 3)
-	fmt.Println(cache.Get(2))
-	cache.Put(4, 4)
-	fmt.Println(cache.Get(1))
-	fmt.Println(cache.Get(3))
-	fmt.Println(cache.Get(4))
-}
+	for i, op := range ops {
+		switch op {
+		case "LRUCache":
+			cache = Constructor(vals[i][0])
+			break
+		case "put":
+			cache.Put(vals[i][0], vals[i][1])
+			break
+		case "get":
+			out = append(out, cache.Get(vals[i][0]))
+			break
+		}
+	}
 
-// need - [null,null,null,null,null,4,3,2,-1,null,-1,2,3,-1,5]
-func tryThree() {
-	cache := Constructor(3)
-
-	cache.Put(1, 1)
-	cache.Put(2, 2)
-	cache.Put(3, 3)
-	cache.Put(4, 4)
-	fmt.Println(cache.Get(4)) // 4
-	fmt.Println(cache.Get(3)) // 3
-	fmt.Println(cache.Get(2)) // 2
-	fmt.Println(cache.Get(1)) // -1
-	fmt.Println("-------")
-
-	cache.Put(5, 5)
-	fmt.Println(cache.Get(1)) // -1
-	fmt.Println(cache.Get(2)) // 2
-	fmt.Println(cache.Get(3)) // 3
-	fmt.Println(cache.Get(4)) // -1
-	fmt.Println(cache.Get(5)) // 5
-}
-
-func tryFour() {
-	cache := Constructor(2)
-	cache.Put(2, 1)
-	cache.Put(1, 1)
-	cache.Put(2, 3)
-	cache.Put(4, 1)
-	fmt.Println(cache.Get(1))
-	fmt.Println(cache.Get(2))
+	fmt.Println(out)
 }
 
 func main() {
-	// tryTwo()
-	fmt.Println("====================")
-	tryThree()
-	// tryFour()
+	// expected:  -1,19,17,-1,-1,-1,5,-1,12,3,5,5,1,-1,30,5,30,-1,-1,24,18,-1,18,-1,18,-1,4,29,30,12,-1,29,17,22,18,-1,20,-1,18,18,20
+	// need:      -1 19 17 -1 -1 -1 5 -1 12 3 5 5 1 -1 -1 5 30 -1 -1 24 18 -1 18 -1 -1 -1 4 29 -1 12 -1 29 17 -1 -1 -1 -1 -1 -1 -1 -1
+	ops := []string{"LRUCache", "put", "put", "put", "put", "put", "get", "put", "get", "get", "put", "get", "put", "put", "put", "get", "put", "get", "get", "get", "get", "put", "put", "get", "get", "get", "put", "put", "get", "put", "get", "put", "get", "get", "get", "put", "put", "put", "get", "put", "get", "get", "put", "put", "get", "put", "put", "put", "put", "get", "put", "put", "get", "put", "put", "get", "put", "put", "put", "put", "put", "get", "put", "put", "get", "put", "get", "get", "get", "put", "get", "get", "put", "put", "put", "put", "get", "put", "put", "put", "put", "get", "get", "get", "put", "put", "put", "get", "put", "put", "put", "get", "put", "put", "put", "get", "get", "get", "put", "put", "put", "put", "get", "put", "put", "put", "put", "put", "put", "put"}
+	vals := [][]int{[]int{10}, []int{10, 13}, []int{3, 17}, []int{6, 11}, []int{10, 5}, []int{9, 10}, []int{13}, []int{2, 19}, []int{2}, []int{3}, []int{5, 25}, []int{8}, []int{9, 22}, []int{5, 5}, []int{1, 30}, []int{11}, []int{9, 12}, []int{7}, []int{5}, []int{8}, []int{9}, []int{4, 30}, []int{9, 3}, []int{9}, []int{10}, []int{10}, []int{6, 14}, []int{3, 1}, []int{3}, []int{10, 11}, []int{8}, []int{2, 14}, []int{1}, []int{5}, []int{4}, []int{11, 4}, []int{12, 24}, []int{5, 18}, []int{13}, []int{7, 23}, []int{8}, []int{12}, []int{3, 27}, []int{2, 12}, []int{5}, []int{2, 9}, []int{13, 4}, []int{8, 18}, []int{1, 7}, []int{6}, []int{9, 29}, []int{8, 21}, []int{5}, []int{6, 30}, []int{1, 12}, []int{10}, []int{4, 15}, []int{7, 22}, []int{11, 26}, []int{8, 17}, []int{9, 29}, []int{5}, []int{3, 4}, []int{11, 30}, []int{12}, []int{4, 29}, []int{3}, []int{9}, []int{6}, []int{3, 4}, []int{1}, []int{10}, []int{3, 29}, []int{10, 28}, []int{1, 20}, []int{11, 13}, []int{3}, []int{3, 12}, []int{3, 8}, []int{10, 9}, []int{3, 26}, []int{8}, []int{7}, []int{5}, []int{13, 17}, []int{2, 27}, []int{11, 15}, []int{12}, []int{9, 19}, []int{2, 15}, []int{3, 16}, []int{1}, []int{12, 17}, []int{9, 1}, []int{6, 19}, []int{4}, []int{5}, []int{5}, []int{8, 1}, []int{11, 7}, []int{5, 2}, []int{9, 28}, []int{1}, []int{2, 2}, []int{7, 4}, []int{4, 22}, []int{7, 24}, []int{9, 26}, []int{13, 28}, []int{11, 26}}
+
+	// -1,-1,2,6
+	// ops = []string{"LRUCache", "get", "put", "get", "put", "put", "get", "get"}
+	// vals = [][]int{[]int{2}, []int{2}, []int{2, 6}, []int{1}, []int{1, 5}, []int{1, 2}, []int{1}, []int{2}}
+
+	// // -1,3
+	// ops = []string{"LRUCache", "put", "put", "put", "put", "get", "get"}
+	// vals = [][]int{[]int{2}, []int{2, 1}, []int{1, 1}, []int{2, 3}, []int{4, 1}, []int{1}, []int{2}}
+
+	// // 1,-1,-1,3,4
+	ops = []string{"LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"}
+	vals = [][]int{[]int{2}, []int{1, 1}, []int{2, 2}, []int{1}, []int{3, 3}, []int{2}, []int{4, 4}, []int{1}, []int{3}, []int{4}}
+
+	setup(ops, vals)
 }
