6701cd19dc5a4937faf7b1e9af582c2a9ae37c44
Zach.Blizzard <zach.blizzard@solera.com>
2020-02-26 13:53:23 -0600

    still nothiing

--- practice/go/channels/channels.go
+++ practice/go/channels/channels.go
@@ -2,32 +2,38 @@ package channels
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 )
 
 // Runner - runner type
 type Runner struct {
-	Stop chan bool
+	stop chan bool
+	wg   *sync.WaitGroup
 }
 
 // NewRunner - returns a new runner
-func NewRunner(stop chan bool) *Runner {
-	return &Runner{stop}
+func NewRunner(stop chan bool, wg *sync.WaitGroup) *Runner {
+	return &Runner{stop, wg}
 }
 
 // Run - runs the stuff...
 func (r *Runner) Run() {
-	fmt.Println("going to start...")
 	for {
-		s := <-r.Stop
+		fmt.Println("going to start...")
+		r.wg.Add(1)
+		s := <-r.stop
 
 		if s {
+			r.wg.Done()
 			fmt.Println("going to stop...")
 			return
 		}
 
 		time.Sleep(5000)
 		fmt.Println("running...")
+
+		r.wg.Done()
 	}
 }
--- practice/go/main.go
+++ practice/go/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"time"
+	"sync"
 
 	"github.com/zach-blizz/code-war-katas/practice/go/channels"
 
@@ -10,13 +11,16 @@ import (
 
 func main() {
 	stop := make(chan bool, 2)
+	wg := new(sync.WaitGroup)
 	stop <- false
 
-	runner := channels.NewRunner(stop)
+	runner := channels.NewRunner(stop, wg)
 	fmt.Println("going to call run")
 	go runner.Run()
 
 	time.Sleep(50000)
 	stop <- true
+
+	wg.Wait()
 	close(stop)
 }
